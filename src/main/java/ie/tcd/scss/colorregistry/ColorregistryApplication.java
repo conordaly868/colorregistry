171af7053eb28fd87470278d7b18ba264577a2af
Conor Daly <dalyc35@tcd.ie>
2024-10-29 00:09:31 +0000

    initial commit

---
+++ src/main/java/ie/tcd/scss/colorregistry/ColorregistryApplication.java
@@ -0,0 +1,13 @@
+package ie.tcd.scss.colorregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColorregistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColorregistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/tcd/scss/colorregistry/ColorregistryApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.tcd.scss.colorregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColorregistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
